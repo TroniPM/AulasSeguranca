24e926f3c0d71fa32b669982ec282e8c7f9bb4da
Paulo Mateus <paulomatew@gmail.com>
2017-11-01 20:39:18 -0300

    Aula 30/out/2017

--- src/Server.java
+++ src/Server.java
@@ -5,35 +5,33 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
 public class Server {
-	private static int contador = 0;
-	
-	public static synchronized int getContador() {
-		int n = contador;
-		increment();
-		return n;
-	}
-	
-	public static void increment() {
-		contador++;
-	}
-	
-	
-	
-	public static void main(String[] args) {
-		try {
-			ServerSocket serverSocket = new ServerSocket(666);
-			System.out.println("Servidor inicializado, aguardando cliente ...");
-			while(true) {
-				Socket socket = serverSocket.accept();
-				new ServerThread(socket).start();
-			}
 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    private static int contador = 0;
 
-	}
+    public static synchronized int getContador() {
+        int n = contador;
+        increment();
+        return n;
+    }
+
+    public static void increment() {
+        contador++;
+    }
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(6666);
+            System.out.println("Servidor inicializado, aguardando cliente ...");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new ServerThread(socket).start();
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/Teste.java
+++ src/Teste.java
@@ -1,3 +1,4 @@
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -8,81 +9,74 @@ import java.util.Date;
 
 import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 
+public class Teste implements Serializable {
 
+    private String seila;
+    private Date data;
 
-public class Teste implements Serializable{
-	private String seila;
-	private Date data;
-	
-	
-	public Teste() {
-		super();
-	}
-	
-	
-	
-	public Teste(String seila) {
-		this.seila = seila;
-		this.data = new Date();
-		
-	}
+    public Teste() {
+        super();
+    }
 
+    public Teste(String seila) {
+        this.seila = seila;
+        this.data = new Date();
 
+    }
 
-	public String getSeila() {
-		return seila;
-	}
-	public void setSeila(String seila) {
-		this.seila = seila;
-	}
-	public Date getData() {
-		return data;
-	}
-	public void setData(Date data) {
-		this.data = data;
-	}
-	
-	public static String convertToString(Teste c) {
-		try {
-			String str;
-			ByteArrayOutputStream baos = new ByteArrayOutputStream();
-			ObjectOutputStream oos = new ObjectOutputStream(baos);
-			oos.writeObject(c);
-			byte[] objeto = baos.toByteArray();
-			str = Base64.encode(objeto);
-			oos.close();
-			return str;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-	public static Teste convertFromString(String str) {
-		try {
-			ByteArrayInputStream bais = new ByteArrayInputStream(Base64.decode(str));
-			ObjectInputStream ois = new ObjectInputStream(bais);
-			return (Teste) ois.readObject();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		return null;
-		
-	}
-	
-	public static void main(String[] args) {
-		Teste t = new Teste("aaaaa");
-		String representacaoStr = Teste.convertToString(t);
-		Teste t2 = Teste.convertFromString(representacaoStr);
-		System.out.println(representacaoStr);
-		System.out.println(t2.getSeila());
-		System.out.println(t2.getData());
-	}
-	
-	
-	
-	
+    public String getSeila() {
+        return seila;
+    }
+
+    public void setSeila(String seila) {
+        this.seila = seila;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public static String convertToString(Teste c) {
+        try {
+            String str;
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(c);
+            byte[] objeto = baos.toByteArray();
+            str = Base64.encode(objeto);
+            oos.close();
+            return str;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Teste convertFromString(String str) {
+        try {
+            ByteArrayInputStream bais = new ByteArrayInputStream(Base64.decode(str));
+            ObjectInputStream ois = new ObjectInputStream(bais);
+            return (Teste) ois.readObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+        Teste t = new Teste("aaaaa");
+        String representacaoStr = Teste.convertToString(t);
+        Teste t2 = Teste.convertFromString(representacaoStr);
+        System.out.println(representacaoStr);
+        System.out.println(t2.getSeila());
+        System.out.println(t2.getData());
+    }
 
 }
