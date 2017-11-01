6c2bae4906712a76b76a6df8b7df451a6e4ed736
Paulo Mateus <paulomatew@gmail.com>
2017-11-01 20:32:05 -0300

    aula 30/out/2017

---
+++ src/Client.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+public class Client {
+	public static void main(String[] args) {
+		try {
+			Socket socket = new Socket("127.0.0.1", 666);
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			
+			Teste t = new Teste("fulano");
+			String msg = Teste.convertToString(t);
+			
+			out.println(msg);
+			
+			
+			
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
